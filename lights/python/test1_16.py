32abaa33dd22ca1559838050eb8888e3065df40d
RJ Marsan <rjmarsan@gmail.com>
2012-03-07 11:58:39 -0600

    Lights API updated and working?

--- lights/python/test1_16.py
+++ lights/python/test1_16.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     import octoapi
     import time
     while True:
-        pix = [(723,1023,923)]*8
+        pix = [(1023,1023,1023)]*24
         octoapi.write(pix)
         time.sleep(0.2)
